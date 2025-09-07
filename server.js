05bc5b8640107e0201dec4ffc6b4ba53752cf59a
Alijassim18 <iceali927@gmail.com>
2025-09-07 18:27:08 +0300

    create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+const app = express();
+
+mongoose.connect(process.env.URL)
+  .then(() => console.log("Database is connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(404).json("Route not found 404");
+});
+
+app.listen(3000, () => {
+  console.log("The backend is listening on port 3000");
+});
