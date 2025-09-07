a3ee6a4d7b79c2b2bdf740f1b11c92c72eac555d
Alijassim18 <iceali927@gmail.com>
2025-09-07 18:31:28 +0300

    create user models

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+
+  Name: {
+    type: String,
+    require: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ["supervisor", "admin","student"],
+    default: "customer"
+  },
+}, { timestamps: true })
+
+
+userSchema.methods.validatePassword = async function (password) {
+  return bcrypt.compare(password, this.passwordHash)
+}
+
+module.exports = mongoose.model("User", userSchema)
